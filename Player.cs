7faed4456249982437bb297107db7aa0ebc1aefd
anbutheslayr <anburathi223@gmail.com>
2024-05-01 00:06:49 +0530

    Ignore

--- Player.cs
+++ Player.cs
@@ -3,19 +3,16 @@ using System;
 
 public class Player : KinematicBody
 {
-	// Declare member variables here. Examples:
-	// private int a = 2;
-	// private string b = "text";
+	
 
-	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		
+		GD.Print("sTART");
 	}
 
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
+ // Called every frame. 'delta' is the elapsed time since the previous frame.
+ 	public override void _Process(float delta)
+ 	{
+     
+ 	}
 }
