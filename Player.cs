dfef66230268c1f70c4cb765eb7a140cedb13edb
anbutheslayr <anburathi223@gmail.com>
2024-05-01 16:55:52 +0530

    Added Sedan

--- Player.cs
+++ Player.cs
@@ -1,18 +1,18 @@
 using Godot;
 using System;
 
-public class Player : KinematicBody
+public class Player : VehicleBody
 {
-	
+    
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        
+    }
 
-	public override void _Ready()
-	{
-		GD.Print("sTART");
-	}
-
- // Called every frame. 'delta' is the elapsed time since the previous frame.
- 	public override void _Process(float delta)
- 	{
-     
- 	}
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(float delta)
+    {
+        
+    }
 }
