f80c5e39085618e6182c931ab85b857feedd009d
anbutheslayr <anburathi223@gmail.com>
2024-05-01 18:36:07 +0530

    Ignore

--- Player.cs
+++ Player.cs
@@ -1,18 +1,28 @@
 using Godot;
 using System;
 
-public class Player : VehicleBody
+public class Player : KinematicBody
 {
-    [Export] public float brake;''
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
-    {
-        
-    }
-
-    // Called every frame. 'delta' is the elapsed time since the previous frame.
-    public override void _Process(float delta)
-    {
-        
-    }
+	
+	[Export] public float brake;
+	[Export] public float Speed;
+	[Export] Camera camera;
+	public float steering = 0;
+	public override void _Ready()
+	{
+		GD.Print("sTART");
+	}
+	public override void _UnhandledInput(InputEvent @event)
+{
+    if (@event is InputEventScreenTouch touch)
+        if (touch.IsPressed())
+		{
+			Viewport View = camera.GetViewport();
+		}
+}
+ // Called every frame. 'delta' is the elapsed time since the previous frame.
+ 	public override void _Process(float delta)
+ 	{
+		 	
+	}
 }
