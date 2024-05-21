61145403a7bce5770f6fd1bdc6e75febd899021d
anbutheslayr <anburathi223@gmail.com>
2024-05-21 14:18:04 +0530

    Improved healthbar

--- Healthbar3d.cs
+++ Healthbar3d.cs
@@ -5,11 +5,14 @@ public class Healthbar3d : Spatial
 {
 	[Export] public string texture_progress_path;
     [Export] public string tween_path;
+    [Export] public string texture_progress_under_path;
     public Tween tween;
 	public TextureProgress texture_progress;
+    public TextureProgress texture_progress_under;
 	public override void _Ready()
 	{
 		texture_progress = GetNode<TextureProgress>(texture_progress_path);
+        texture_progress_under = GetNode<TextureProgress>(texture_progress_under_path);
 		texture_progress.Value = 100;
         tween = GetNode<Tween>(tween_path);
 	}
@@ -17,8 +20,10 @@ public class Healthbar3d : Spatial
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public void Change_Health(int health)
 	{
-		// texture_progress.Value = health;
-        tween.InterpolateProperty(texture_progress , "value" , texture_progress.Value , health, 0.3f, Tween.TransitionType.Back, Tween.EaseType.Out);
+		texture_progress.Value = health;
+        tween.InterpolateProperty(texture_progress , "value" , texture_progress.Value , health, 0.2f, Tween.TransitionType.Elastic, Tween.EaseType.In);
+        tween.Start();
+        tween.InterpolateProperty(texture_progress_under , "value" , texture_progress_under.Value , health, 0.5f, Tween.TransitionType.Cubic, Tween.EaseType.Out);
         tween.Start();
 	}
 }
