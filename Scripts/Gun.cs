a4d6039e60cb2b3fb54068803695ea6fb179b45a
anbutheslayr <anburathi223@gmail.com>
2024-05-22 15:33:10 +0530

    Added bullet decal

--- Scripts/Gun.cs
+++ Scripts/Gun.cs
@@ -1,13 +1,25 @@
 using Godot;
+using System;
 using System.Collections.Generic;
 
 public class Gun : Spatial
 {
+    [Export] public string RayCastPath;
+    [Export] public string AnimPath;
+    public MeshInstance gun;
+    [Export] public string gun_path;
+    public AnimationPlayer anim;
+    public RayCast ray_cast;
     public List<Spatial> enemies = new List<Spatial>();
-
+    public PackedScene decal;
+    [Export] public string decal_path;
     public override void _Ready()
     {
-        
+        ray_cast = GetNode<RayCast>(RayCastPath);
+        ray_cast.Enabled = true;
+        anim = GetParent().GetNode<AnimationPlayer>(AnimPath);
+        gun = GetNode<MeshInstance>(gun_path);
+        decal = GD.Load<PackedScene>(decal_path);
     }
 
     public void OnDetection(Node body)
@@ -28,13 +40,15 @@ public class Gun : Spatial
     // Called every frame. 'delta' is the elapsed time since the previous frame.
     public override void _Process(float delta)
     {
-        if(enemies.Count > 0)
+        if(enemies.Count > 0 && GetClosestEnemy() != null)
         {
             Spatial closest_enemy = GetClosestEnemy();
-            if(closest_enemy != null)
-            {
-                AimAt(closest_enemy.GlobalTransform.origin, Vector3.Up);
-            }
+            
+                if(anim.CurrentAnimation != "Shoot")
+                {
+                    gun.LookAt(AimAt(closest_enemy.GlobalTransform.origin), Vector3.Up);
+                    anim.Play("Shoot");
+                }
         }
     }
     public Spatial GetClosestEnemy()
@@ -52,9 +66,26 @@ public class Gun : Spatial
         }
         return closest_enemy;
     }
-    private void AimAt(Vector3 target, Vector3 up)
+    private Vector3 AimAt(Vector3 target)
     {
+        
         Vector3 direction = target - GlobalTransform.origin;
-        LookAt(GlobalTransform.origin + direction, up);
+        return GlobalTransform.origin - direction;
+    }
+    private void OnShoot()
+    {
+        if( ray_cast.IsColliding() )
+        {
+            var random = new Random();
+            GD.Print("Shot");
+            GD.Print(ray_cast.GetCollider());
+            var b = decal.Instance() as Spatial;
+            (ray_cast.GetCollider() as Area).AddChild(b);
+            var transform = b.GlobalTransform;
+            transform.origin = ray_cast.GetCollisionPoint();
+            b.GlobalTransform = transform;
+            b.LookAt(ray_cast.GetCollisionPoint() + ray_cast.GetCollisionNormal(), Vector3.Up);
+        }
+
     }
 }
