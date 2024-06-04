15ee582c39b8d859b41fca9b4f13332b46924373
anbutheslayr <anburathi223@gmail.com>
2024-06-04 17:49:42 +0530

    Ignore

--- Scripts/Menu.cs
+++ Scripts/Menu.cs
@@ -6,12 +6,9 @@ public class Menu : Control
     public VBoxContainer vb1;
     public VBoxContainer vb2;
     public resolution Res;
-    public DynamicFont title_font;
-    public Theme theme;
     public override void _Ready()
     {
-        title_font = GD.Load<DynamicFont>("res://Scenes/FONT.tres");
-        theme = GD.Load<Theme>("res://Scenes/Theme.tres");
+
         Res = GD.Load<resolution>("res://Interface/Res.tres");
         if (Res.res != Vector2.Zero)
         {
@@ -43,9 +40,5 @@ public class Menu : Control
         vb1.AddConstantOverride("separation", (int)(resolution.y/1080*70));
         vb2 = GetNode<VBoxContainer>("MarginContainer/HBoxContainer/VBoxContainer");
         vb2.AddConstantOverride("separation", (int)(resolution.y/1080*30));
-
-        title_font.Size = (int)(resolution.x/1920*64);
-        // theme.DefaultFont.Set("size", (resolution.x/1920)*30);
-        // theme.DefaultFont.Set("outline_size", (resolution.x/1920)*4);
     }
 }
