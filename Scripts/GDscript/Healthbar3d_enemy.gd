b8c7f140cc8d32692463c9bc6a69fab45a062ef8
anbutheslayr <anburathi223@gmail.com>
2025-03-14 14:19:08 +0530

    Almost there

---
+++ Scripts/GDscript/Healthbar3d_enemy.gd
@@ -0,0 +1,15 @@
+extends Node3D
+
+
+@export var texture_progress_path : NodePath
+@export var texture_progress_under_path : NodePath
+@onready var texture_progress : TextureProgressBar = get_node(texture_progress_path)
+@onready var texture_progress_under : TextureProgressBar = get_node(texture_progress_under_path)
+
+var tween : Tween = Tween.new()
+
+func change_health(health : int):
+    
+    tween.tween_property(texture_progress, "value", health, 0.15).set_trans(Tween.TRANS_ELASTIC).set_ease(Tween.EASE_IN)
+    tween.tween_property(texture_progress_under, "value", health, 0.7).set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_IN)
+
