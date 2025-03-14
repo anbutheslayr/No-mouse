32d552f3bab9b5c230a9c9ab9fd73cd046aa7bf6
anbutheslayr <anburathi223@gmail.com>
2025-03-14 10:43:21 +0530

    Still porting. When will this end?

---
+++ Scripts/GDscript/Menu.gd
@@ -0,0 +1,24 @@
+extends Control
+
+@onready var level_select : Control = get_node("Select level")
+
+func on_play_pressed():
+    get_parent().click()
+    hide()
+    level_select.show()
+
+func on_settings_pressed():
+    get_parent().click()
+    hide()
+    get_parent().settings.show()
+
+func on_quit_pressed():
+    get_parent().backed()
+    get_tree().quit()
+
+func on_insta_pressed():
+    OS.shell_open("https://www.instagram.com/anbu_the_coder/profilecard/?igsh=MTBmbm83Z3hwcjI3bw%3D%3D")
+
+
+
+
--- Scripts/GDscript/resolution.gd
+++ Scripts/GDscript/resolution.gd
@@ -4,11 +4,11 @@ class_name Resolution
 @export var res : Vector2i = Vector2i(1920, 1080)
 @export var res_int : int = 0
 @export var shadows : bool = true
-@export var ShadowQuality : int = 0
-@export var Glow : bool = true
-@export var NoOfEnemies : int = 6
+@export var shadow_quality : int = 0
+@export var glow : bool = true
+@export var no_of_enemies : int = 6
 @export var volume : int = 0
-@export var Drift_points : float = 0
+@export var drift_points : float = 0
 @export var max_worlds : int = 2
 @export var cur_world : int = 1
 @export var difficulty : int = 0
